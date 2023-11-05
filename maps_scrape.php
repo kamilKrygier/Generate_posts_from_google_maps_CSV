5072ee20ae5a58ef2af2ad53145a57a342cb97b7
Kamil Krygier <32956194+kamilKrygier@users.noreply.github.com>
2023-11-05 18:08:44 +0100

    Update maps_scrape.php

--- maps_scrape.php
+++ maps_scrape.php
@@ -1,8 +1,6 @@
 <?php
 
-// TODO Add batch processing of Google Places Scrape (city by city, because this script requires extended PHP max_execution_time)
-
-// All main Google Places Categories
+// All main Google Places Categories (10.2023)
 $place_category = ["accounting","airport","amusement_park","aquarium","art_gallery","atm","bakery","bank","bar","beauty_salon","bicycle_store","book_store","bowling_alley","bus_station","cafe","campground","car_dealer","car_rental","car_repair","car_wash","casino","cemetery","church","city_hall","clothing_store","convenience_store","courthouse","dentist","department_store","doctor","drugstore","electrician","electronics_store","embassy","fire_station","florist","funeral_home","furniture_store","gas_station","gym","hair_care","hardware_store","hindu_temple","home_goods_store","hospital","insurance_agency","jewelry_store","laundry","lawyer","library","light_rail_station","liquor_store","local_government_office","locksmith","lodging","meal_delivery","meal_takeaway","mosque","movie_rental","movie_theater","moving_company","museum","night_club","painter","park","parking","pet_store","pharmacy","physiotherapist","plumber","police","post_office","primary_school","real_estate_agency","restaurant","roofing_contractor","rv_park","school","secondary_school","shoe_store","shopping_mall","spa","stadium","storage","store","subway_station","supermarket","synagogue","taxi_stand","tourist_attraction","train_station","transit_station","travel_agency","university","veterinary_care","zoo"];
 
 // Process the code after form is submitted
@@ -99,9 +97,6 @@ if (isset($_POST['submit']) && isset($_POST['place_category'])) {
     if (file_exists($output_path)) {
         unlink($output_path);
     }
-
-    // header("Content-Type: text/csv; charset=utf-8");
-    // header("Content-Disposition: attachment; filename=$slugified_place_category.csv");
 
     $output = fopen($output_path, "w");
 
